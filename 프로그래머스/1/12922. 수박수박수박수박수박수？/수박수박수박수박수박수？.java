33a0c64c249ccf56afc85bb2a1e045c78c1e5175
kshksh723 <67942699+kshksh723@users.noreply.github.com>
2024-03-25 21:30:07 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 2.75 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -1,9 +1,6 @@
 class Solution {
     public String solution(int n) {
         String answer = "";
-           for(int i = 0; i<n; i++){
-            answer +=(i%2 == 0) ? "수":"박";
-        }
         return answer;
     }
 }
