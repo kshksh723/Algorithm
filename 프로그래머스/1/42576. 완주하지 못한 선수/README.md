538fd253b4c3895c6cdf7dbbfd1a6111f068ca5a
kshksh723 <67942699+kshksh723@users.noreply.github.com>
2024-08-13 18:34:05 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 87.51 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
-메모리: 96.7 MB, 시간: 278.11 ms
+메모리: 111 MB, 시간: 87.51 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 13일 18:01:19
+2024년 08월 13일 18:34:07
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,7 +1,9 @@
 import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
-           String answer = "";
+      /* 
+      ✅ sorting해서 문제 풀기 
+      String answer = "";
         // 1. 두 배열을 정렬한다
         Arrays.sort(participant);
         Arrays.sort(completion); // 두 배열이 알파벳 순서대로 되어있다는 것을 알 수 있다
@@ -16,10 +18,42 @@ class Solution {
         // 3. 여기까지 왔다면 마지막 주자가 완주하지 못한 선수다
      
         return participant[i];
+        
+        */
+//         해시로 문제 풀기
+        String answer = "";
+        // 1, HashMap을 만든다 (participant)
+        HashMap<String, Integer> map = new HashMap<>();
+        for(String player : participant)
+            map.put(player, map.getOrDefault(player, 0) + 1);
+        // 2. HashMap을 뺀다(completion)
+        for(String player : completion)
+            map.put(player, map.get(player) - 1); 
+        // 3. value가 0이 아닌 마지막 주자를 찾는다
+        for(String key : map.keySet()){
+            if(map.get(key) != 0){
+                answer = key;
+                break; 
+            }
+        }
+        return answer;
     }
 }
 
+/*
+getOrDefault
+-  찾는 키가 존재한다면 찾는 키의 값을 반환하고 없다면 기본 값을 반환하는 메서드
+ 
+출처: https://junghn.tistory.com/entry/JAVA-Map-getOrDefault-이란-사용법-및-예제 [코딩 시그널:티스토리]
+getOrDefault(Object key, V DefaultValue)
+매개 변수 : 이 메서드는 두 개의 매개 변수를 허용합니다.
 
+key : 값을 가져와야 하는 요소의 키입니다.
+defaultValue : 지정된 키로 매핑된 값이 없는 경우 반환되어야 하는 기본값입니다.
+
+반환 값 : 찾는 key가 존재하면 해당 key에 매핑되어 있는 값을 반환하고, 그렇지 않으면 디폴트 값이 반환됩니다
+
+*/
 
 /*
 solution 도출하기 
@@ -28,6 +62,5 @@ solution 도출하기
 
 마라톤에 참여한 선수의 배열 participant고, 완주한 선수들의 이름이 담긴 배열은 completion이다 
 
-왜 sorting을 하는 것인지 
 
 */
