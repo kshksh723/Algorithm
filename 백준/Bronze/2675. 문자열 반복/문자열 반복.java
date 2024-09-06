14f92b03ac66ac7c0140a2889db1e2e1328bc8cf
kshksh723 <67942699+kshksh723@users.noreply.github.com>
2024-09-06 17:06:42 +0900

    [Bronze II] Title: 문자열 반복, Time: 176 ms, Memory: 17640 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.java
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.java
@@ -4,8 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // 보통 문자열을 반복해서 쓸 땐 for문을 사용해서 반복했지만 repeat를 사용하여 편하게 문자열을 반복 나열 할 수 있다
-        // repeat() : String의 메서드, String 문자열을 파라미터의 주어진 횟수만큼 반복
         int T = sc.nextInt();
         for(int i = 0; i < T; i++) {
 
