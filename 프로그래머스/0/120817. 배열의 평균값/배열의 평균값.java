01b9a648648face604ef777964f2b51efd567eea
김승혜 <67942699+kshksh723@users.noreply.github.com>
2025-12-01 13:04:25 +0900

    [level 0] Title: 배열의 평균값, Time: 0.04 ms, Memory: 91.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public double solution(int[] numbers) {
+        int sum = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+
+        return (double) sum / numbers.length;
+    }
+}
